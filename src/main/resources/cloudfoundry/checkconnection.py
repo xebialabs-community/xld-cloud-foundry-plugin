5043a24302b8c7551417a7d1721c2f2c1c99f102
Joris De Winne <jdewinne@localhost.localdomain>
2014-09-23 19:50:59 +0200

    Added jython delegate

--- src/main/resources/cloudfoundry/checkconnection.py
+++ src/main/resources/cloudfoundry/checkconnection.py
@@ -7,32 +7,7 @@
 from cloudfoundry.util import CFClientUtil
 import sys
 
-for arg in sys.argv:
-    print arg
-
-class Container:
-   def __init__(self,apiEndpoint,username,password,org,spaceName):
-      self.apiEndpoint = apiEndpoint
-      self.username = username
-      self.password = password
-      self.org = org
-      self.spaceName = spaceName
-
-   def getProperty(self,propertyName):
-      if propertyName == "apiEndpoint":
-          return self.apiEndpoint
-      if propertyName == "username":
-          return self.username
-      if propertyName == "password":
-          return self.password
-      if propertyName == "org":
-          return self.org
-      if propertyName == "spaceName":
-          return self.spaceName
-
-container = Container(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5])
-
-cfClient = CFClientUtil.createClient(container)
+cfClient = CFClientUtil.createClient(thisCi)
 
 if cfClient is None:
 	sys.exit("Could not connect to cloudfoundry space")
